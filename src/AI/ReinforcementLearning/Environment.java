dca8ef5ee97308100752554c1aa25f5e25586f74
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2023-04-14 16:31:09 -0700

    Refactor Changes

--- src/AI/ReinforcementLearning/Environment.java
+++
@@ -1,9 +0,0 @@
-package AI.ReinforcementLearning;
-
-import AI.utils.NetworkInputs;
-
-public interface Environment {
-    NetworkInputs get();
-    double react(NetworkInputs action);
-    double getMaxReward();
-}
--- src/AI/ReinforcementLearning/Policy.java
+++
@@ -1,8 +0,0 @@
-package AI.ReinforcementLearning;
-
-import AI.utils.NetworkInputs;
-
-public interface Policy {
-    void modifyAction(NetworkInputs action);
-    NetworkInputs getErrors(NetworkInputs action, NetworkInputs newAction, double error);
-}
